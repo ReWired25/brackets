ed83f9c5cc0d82adb19908973fa8f1feb5082676
Nikita K <w.feelingalright.p@yandex.ru>
2022-02-04 00:26:55 +0300

    feat: add brackets algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checkStr = '';
+  let arr = bracketsConfig;
+
+  for (let i = 0; i < str.length; i++) {
+      for (let key of arr) {
+          if (str[i] === key[1] && checkStr[checkStr.length - 1] === key[0]) {
+              checkStr = checkStr.slice(0, checkStr.length - 1);
+          } else if (str[i] === key[0]) {
+              checkStr += str[i];
+          } else if (str[i] === key[1]) {
+            checkStr += str[i];
+          }
+      }
+  }
+
+  return checkStr.length === 0 ? true : false;
 }
